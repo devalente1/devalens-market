83e4c235115cdbc4dbb08e784e19a477384d8b20
Alexander Morales <devalente747@gmail.com>
2024-10-21 22:37:50 -0500

    Se agregaron archivos de configuración para diversos entornos

---
+++ src/main/java/com/devalens/market/DevalensMarketApplication.java
@@ -0,0 +1,13 @@
+package com.devalens.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevalensMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevalensMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devalens/market/DevalensMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalens.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevalensMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
